e7b28a90ded4230d50111a45882b85809abb324e
Manesh George <manesh.george@livanova.com>
2021-09-02 16:59:47 -0400

    deploy to docker

--- pipeline-info/deploy.sh
+++ pipeline-info/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build . -t quay.io/livanova/pipeline-info-resource
-docker push  quay.io/livanova/pipeline-info-resource
+docker build . -t mglivn/pipeline-info-resource
+docker push  mglivn/pipeline-info-resource
